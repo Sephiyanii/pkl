be22dc1416f4f058e42921292cfb1ac1f811633c
Sephiyanii <sephiyanigabriela@gmail.com>
2019-07-06 09:09:25 +0700

    tempalte sepi

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('gallery', function () {
+    return view('gallery');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('about', function () {
+    return view('about');
 });
